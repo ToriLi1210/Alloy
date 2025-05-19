21c466246c946dab0c26bf1c1ab10ae65b9b858d
ToriLi1210 <hanying1@student.unimelb.edu.au>
2025-05-19 16:13:28 +1000

    Task3.1 b

--- mystringtokeniser.ads
+++ mystringtokeniser.ads
@@ -30,12 +30,22 @@ package MyStringTokeniser with SPARK_Mode is
    -- Why it is necessary:
    --    This ensures that accessing elements Tokens(1..Count) is safe and will not go out of bounds.
    --    Without this, SPARK cannot verify array accesses like T(I), and a proof failure like "array index check might fail" may occur.
-     Post => Count <= Tokens'Length and
-     (for all Index in Tokens'First..Tokens'First+(Count-1) =>
-          (Tokens(Index).Start >= S'First and
-          Tokens(Index).Length > 0) and then
-            Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start);
-   --
+
+     Post => Count <= Tokens'Length;
+      and (for all Index in Tokens'First..Tokens'First+(Count-1) =>
+      (Tokens(Index).Start >= S'First and
+       Tokens(Index).Length > 0) and then
+           Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start);
+
+--  102:87 medium: overflow check might fail [possible explanation: postcondition of call at line 92 should mention Tokens (for argument T)]
+--  102:53 medium: precondition might fail, cannot prove From <= To [possible explanation: postcondition of call at line 92 should mention Tokens (for argument T)]
+--          102:99 medium: range check might fail [possible explanation: postcondition of call at line 92 should mention Tokens (for argument T)]
+--  What part is saying:
+--  for every token to be valid stored in Tokenarray, it must start within the bound of string S,
+--  the length of token must be non-zero and ends within the bound of string s, means the token exist in the string.
+--  Why it is necessary:
+--     This ensures that every token that will be stored in tokennarray must be a valid slice of string S.
+--     Without this, SPARK would report potential range check, overflow, or precondition violations.
 
 
 end MyStringTokeniser;
