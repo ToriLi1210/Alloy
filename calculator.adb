ec580219133a8565690f586a257c73aa8156031e
ToriLi1210 <hanying1@student.unimelb.edu.au>
2025-05-23 23:41:45 +1000

    fix(task1.2):replace Integer with correct Int32 ,alias from Interfaces.Integer_32 type"
    compile pass！！！

--- calculator.adb
+++ calculator.adb
@@ -1,19 +1,8 @@
---  MyString.adb
---
---  This file implements the operations defined in the MyString specification.
---  MyString is a SPARK-compatible bounded string type that avoids dynamic-length
---  operations, which are not verifiable in SPARK.
---
---  The functions provided here allow safe construction, conversion, comparison,
---  and manipulation of strings with a maximum length defined at instantiation.
---
---  This implementation includes contracts and loop invariants to support formal
---  verification in SPARK, except for routines like Get_Line which use Ada.Text_IO
---  and are excluded from analysis.
---
---  Do not modify this file.
-
 with Ada.Text_IO;
+use Ada.Text_IO;
+with MemoryStore;
+with Interfaces;
+use Interfaces;
 
 package body Calculator is
    ---------------------------------------------------------------------------
@@ -21,76 +10,214 @@ package body Calculator is
    ---------------------------------------------------------------------------
    procedure Init(C : out Calculator; Master_PIN: in PIN.PIN) is
    begin
-
+      -- The calculator begins in the locked state.
       C.Locked:=True;
+      -- User needs to supply master pin
       C.Masterpin:=Master_PIN;
+      -- The size of operand stack is 0
       C.Length:=0;
       C.Operand_stack:=(others=>0);
 
    end Init;
 
-
    -- "+"
-   procedure Add(Number_1: in Integer; Number_2: in Integer;Result: out Integer) is
+   procedure Addition(Number_1: in Int32; Number_2: in Int32;Result: out Int32) is
    begin
-   end Add;
+      Result:= Number_1+Number_2;
+   end Addition;
 
    -- "-"
-   procedure Minus(Number_1: in Integer; Number_2: in Integer;Result: out Integer) is
+   procedure Subtraction(Number_1: in Int32; Number_2: in Int32;Result: out Int32) is
    begin
-   end Minus;
+      Result:= Number_1-Number_2;
+
+   end Subtraction;
 
    -- "*"
-   procedure Multiple(Number_1: in Integer; Number_2: in Integer;Result: out Integer) is
+   procedure Multiplication(Number_1: in Int32; Number_2: in Int32;Result: out Int32) is
    begin
-   end Multiple;
+      Result:= Number_1*Number_2;
+   end Multiplication;
 
    -- "/"
-   procedure Divide(Number_1: in Integer; Number_2: in Integer;Result: out Integer) is
+   procedure Division(Number_1: in Int32; Number_2: in Int32;Result: out Int32) is
    begin
-   end Divide;
+      Result:= Number_1/Number_2;
+   end Division;
 
    -- push1 <NAME>
-   procedure Push_1(C:in out Calculator;Number_1:in Integer) is
-      begin
+   -- Push a number into the operand stack(Read and Modified) with a Int32(Read only)
+   procedure Push_1(C:in out Calculator;Number_1:in Int32) is
+   begin
+      C.Length:=C.Length+1;
+      -- since the index of operand stack starts with 1
+      C.Operand_stack(C.Length):=Number_1;
    end Push_1;
 
    -- push2 <NAME> <NAME>
-   procedure Push_2(C:in out Calculator;Number_1:in Integer;Number_2:in Integer) is
+   -- Push 2 numbers into the operand stack with sequence(Read and Modified) with a Int32(Read only)
+   procedure Push_2(C:in out Calculator;Number_1:in Int32;Number_2:in Int32) is
    begin
+      Push_1(C,Number_1);
+      Push_1(C,Number_2);
    end Push_2;
 
    -- pop
-   procedure Pop(C:in out Calculator;Result:out Integer)is
+   -- remove 1 number from the top of operand stack and stored in Result
+   procedure Pop(C:in out Calculator;Result:out Int32)is
    begin
+      Result:=C.Operand_stack(C.Length);
+      C.Length:=C.Length-1;
    end Pop;
 
 
    -- loadFrom <NAME>; loads the value stored at memory location loc and pushes it onto the operand stack
    procedure Load_From(C:in out Calculator;D : in out MemoryStore.Database; Loc: in MemoryStore.Location_Index) is
    begin
+      declare
+         -- local variable
+         Val:Int32;
+      begin
+         -- the value exist at memory location
+         if MemoryStore.Has(D,Loc) then
+            Val := MemoryStore.Get(D,Loc);
+            Push_1(C,Val);
+         else
+            Put_Line("No value has been stored in memory location");
+         end if;
+      end;
    end Load_From;
 
 
    -- storeTo <NAME>; pop the value from the top of the operand stack and stores it into memory location loc
    procedure Store_To(C:in out Calculator;D: in out MemoryStore.Database; Loc: in MemoryStore.Location_Index) is
    begin
+      declare
+         Val:Int32;
+      begin
+         -- no value exist at memory location
+         if not MemoryStore.Has(D,Loc) then
+            Pop(C,Val);
+            MemoryStore.Put(D,Loc,Val);
+         else
+            Put_Line("A value has already been stored in memory location");
+         end if;
+      end;
+
    end Store_To;
 
 
    -- unlock <NAME>
    procedure Unlock(C:in out Calculator;P: in PIN.PIN) is
    begin
+      -- unlock if pin is correct
+      if Is_PIN(C,P) then
+         C.Locked := False;
+      else
+         Put_Line("Invalid Pin");
+      end if;
+
    end Unlock;
 
-   -- lock <NAME>
-   procedure Lock(C:in out Calculator;P: in out PIN.PIN) is
+   -- lock <NAME> The “lock” command allows updating the master PIN
+   procedure Lock(C:in out Calculator;P: in PIN.PIN) is
    begin
+      -- modify master pin if user locks with new Pin
+      C.Masterpin := P;
+      C.Locked := True;
    end Lock;
 
    -- identify the command and call corresponding operation method
    procedure Calculation(C: in out Calculator; Operation: String) is
    begin
+      declare
+         Val_1:Int32;
+         Val_2:Int32;
+         Result:Int32;
+         Temp:Long_Long_Integer;
+         Max_Int32: constant Int32:=Int32'Last;
+         Min_Int32: constant Int32:=Int32'First;
+
+      begin
+
+         -- exit if locked
+         pragma Assert (not Is_Locked(C));
+
+         -- pop the top two values from the operand stack
+         Pop(C,Val_1);
+         Pop(C,Val_2);
+
+         -- Addition
+         if Operation = "+" then
+            -- convert from Int32 to Long Long Int32
+            Temp:=Long_Long_Integer(Val_1)+Long_Long_Integer(Val_2);
+
+            -- ensure the result not overflow
+            if Temp > Long_Long_Integer(Max_Int32) or
+                  Temp < Long_Long_Integer(Min_Int32) then
+
+                   -- push back to operand stack
+                   Push_2(C,Val_1,Val_2);
+                Put_Line("Invald operation: Addition overflow");
+            else
+                Addition(Val_1,Val_2,Result);
+                Push_1(C,Result);
+            end if;
+
+         -- Subtraction
+         elsif Operation = "-" then
+            -- convert from Int32 to Long Long Int32
+            Temp:=Long_Long_Integer(Val_1)-Long_Long_Integer(Val_2);
+
+            -- ensure the result not overflow
+            if Temp > Long_Long_Integer(Max_Int32) or
+                  Temp < Long_Long_Integer(Min_Int32) then
+
+                   -- push back to operand stack
+                   Push_2(C,Val_1,Val_2);
+                Put_Line("Invald operation: Subtraction overflow");
+            else
+                Subtraction(Val_1,Val_2,Result);
+                Push_1(C,Result);
+            end if;
+
+         -- Multiplication
+         elsif Operation = "*" then
+            -- convert from Int32 to Long Long Int32
+            Temp:=Long_Long_Integer(Val_1)*Long_Long_Integer(Val_2);
+
+            -- ensure the result not overflow
+            if Temp > Long_Long_Integer(Max_Int32) or
+                  Temp < Long_Long_Integer(Min_Int32) then
+
+                   -- push back to operand stack
+                   Push_2(C,Val_1,Val_2);
+                Put_Line("Invald operation: Multiplication overflow");
+            else
+                Multiplication(Val_1,Val_2,Result);
+                Push_1(C,Result);
+            end if;
+
+         -- Division
+         elsif Operation = "/" then
+                   -- convert from Int32 to Long Long Int32
+            Temp:=Long_Long_Integer(Val_1)/Long_Long_Integer(Val_2);
+
+            -- ensure the result not overflow
+            if Temp > Long_Long_Integer(Max_Int32) or
+                  Temp < Long_Long_Integer(Min_Int32) then
+
+                   -- push back to operand stack
+                   Push_2(C,Val_1,Val_2);
+                Put_Line("Invald operation: Multiplication overflow");
+            else
+                Division(Val_1,Val_2,Result);
+                Push_1(C,Result);
+            end if;
+         end if;
+      end;
+
+
    end Calculation;
 
 
@@ -101,59 +228,18 @@ package body Calculator is
    -- the Pin
    function Is_PIN(C : in Calculator;P: in PIN.PIN) return Boolean is
    begin
+      -- PIN equality
+      if PIN."="(P,C.Masterpin) then
+         return True;
+      else
+         return False;
+      end if;
+
    end Is_PIN;
 
 
---     function Has (D : Database; Loc : Location_Index) return Boolean is
---       (D.Mem (Loc).Valid);
---
---     function Get (D : Database; Loc : Location_Index) return Int32 is
---       (D.Mem (Loc).Val);
---
---     function Length (D : Database) return Natural is
---       (D.Cnt);
 
-   ---------------------------------------------------------------------------
-   --  Update operations
-   ---------------------------------------------------------------------------
---     procedure Put
---       (D   : in out Database;
---        Loc : in     Location_Index;
---        Val : in     Int32) is
---     begin
---        if not D.Mem (Loc).Valid then
---           -- inserting a brand-new entry
---           D.Cnt := D.Cnt + 1;
---        end if;
---        D.Mem (Loc).Valid := True;
---        D.Mem (Loc).Val   := Val;
---     end Put;
---
---     procedure Remove
---       (D   : in out Database;
---        Loc : in     Location_Index) is
---     begin
---        if D.Mem (Loc).Valid then
---           D.Mem (Loc).Valid := False;
---           D.Cnt             := D.Cnt - 1;
---        end if;
---     end Remove;
 
-   ---------------------------------------------------------------------------
-   --  Pretty-print for the "list" command
-   ---------------------------------------------------------------------------
---     procedure Print (D : Database) is
---     begin
---        for Loc in Location_Index loop
---           if D.Mem (Loc).Valid then
---              Ada.Text_IO.Put ("   ");
---              Ada.Integer_Text_IO.Put (Loc, Width => 0);
---              Ada.Text_IO.Put (" => ");
---              Ada.Integer_Text_IO.Put (Integer (D.Mem (Loc).Val), Width => 0);
---              Ada.Text_IO.New_Line;
---           end if;
---        end loop;
---     end Print;
 
 
 
--- calculator.ads
+++ calculator.ads
@@ -1,37 +1,46 @@
 with PIN;
 with MemoryStore;
-generic
-   Calculator_Stack_Capacity : Positive;
-   -- The capacity of the calculator’s operand stack is 512.
+with Interfaces;  -- supplies Int32_32 type
+
+
 
 package Calculator with SPARK_Mode is
 
    type Calculator is private;
+   -- If we want to change the type in the future
+   -- we only need to update one place — this improves maintainability.
+
+    -- The capacity of the calculator’s operand stack is 512.
+   Calculator_Stack_Capacity : constant Positive := 512;
+
+   --  The calculator specification allows "any 32-bit signed Int32".
+   subtype Int32 is Interfaces.Integer_32;  -- range   -2_147_483_648 ..  2_147_483_647
+
    -- Initialises the calculator with the master pin
    -- PIN.PIN  the type PIN defined inside the package PIN
    procedure Init   (C : out Calculator; Master_PIN: in PIN.PIN);
 
    -- "+"
-   procedure Add(Number_1: in Integer; Number_2: in Integer;Result: out Integer);
+   procedure Addition(Number_1: in Int32; Number_2: in Int32;Result: out Int32);
 
    -- "-"
-   procedure Minus(Number_1: in Integer; Number_2: in Integer;Result: out Integer);
+   procedure Subtraction(Number_1: in Int32; Number_2: in Int32;Result: out Int32);
 
    -- "*"
-   procedure Multiple(Number_1: in Integer; Number_2: in Integer;Result: out Integer);
+   procedure Multiplication(Number_1: in Int32; Number_2: in Int32;Result: out Int32);
 
    -- "/"
-   procedure Divide(Number_1: in Integer; Number_2: in Integer;Result: out Integer);
+   procedure Division(Number_1: in Int32; Number_2: in Int32;Result: out Int32);
 
 
    -- push1 <NAME>
-   procedure Push_1(C:in out Calculator;Number_1:in Integer);
+   procedure Push_1(C:in out Calculator;Number_1:in Int32);
 
    -- push2 <NAME> <NAME>
-   procedure Push_2(C:in out Calculator;Number_1:in Integer;Number_2:in Integer);
+   procedure Push_2(C:in out Calculator;Number_1:in Int32;Number_2:in Int32);
 
    -- pop
-   procedure Pop(C:in out Calculator;Result:out Integer);
+   procedure Pop(C:in out Calculator;Result:out Int32);
 
    -- loadFrom <NAME>; loads the value stored at memory location loc and pushes it onto the operand stack
    procedure Load_From(C:in out Calculator;D : in out MemoryStore.Database; Loc: in MemoryStore.Location_Index);
@@ -43,14 +52,13 @@ package Calculator with SPARK_Mode is
    procedure Unlock(C:in out Calculator;P: in PIN.PIN);
 
    -- lock <NAME>
-   procedure Lock(C:in out Calculator;P: in out PIN.PIN);
+   procedure Lock(C:in out Calculator;P: in PIN.PIN);
 
    -- identify the command and call corresponding operation method
    procedure Calculation(C: in out Calculator; Operation: String);
 
    -- the Pin
    function Is_PIN(C : in Calculator;P: in PIN.PIN) return Boolean;
-
 
 
    -------already implementated in private section -------
@@ -72,8 +80,8 @@ package Calculator with SPARK_Mode is
 
 
 private
-   -- calculator's operand stack; stored Integer
-   type Operand_Stack_Array is array(Positive range 1..Calculator_Stack_Capacity) of Integer;
+   -- calculator's operand stack; stored Int32
+   type Operand_Stack_Array is array(Positive range 1..Calculator_Stack_Capacity) of Int32;
    -- the parameter
    type Calculator is record
       Length : Natural range 0..Calculator_Stack_Capacity;
@@ -88,6 +96,5 @@ private
    function Is_Locked(C : in Calculator) return Boolean is (C.Locked);
    function Get_Pin(C : in Calculator) return PIN.PIN is
         (C.Masterpin);
-
 
 end Calculator;
